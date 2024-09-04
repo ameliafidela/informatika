6fb4e1f2ac1ab5b556af12c27feae7e4176287ca
fidela <145947359+ameliafidela@users.noreply.github.com>
2024-09-04 11:27:13 +0700

    Add files via upload

---
+++ Implementasi1 Amelia Fidela.py
@@ -0,0 +1,13 @@
+print("Hello Fidela!")
+print("Belajar Python dari Nol")
+nama = "python pemula"
+print("Hello"); print("Fidela"); print("Tutorial Python untuk Pemula")
+nama_depan = "python";nama_belakang = "pemula"
+"Belajar Pemrograman Python sampai Bisa"penulis = 'python pemula'
+"Belajar Membuat Program Python"
+if username == 'pythonpemula'
+:
+print("Selamat Datang Fidela")
+print("Silakan ambil tempat duduk")
+variabel_ku = "ini isi variabel"
+variabel2 = 20
